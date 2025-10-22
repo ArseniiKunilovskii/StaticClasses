a5ea124d19965c34d8e6448b88cc1679e78676be
arsenii <arseniy.kunilovskiy@gmail.com>
2025-10-22 13:19:05 -0400

    NameFormatterTest is finished All methods are tested

--- src/test/java/com/pluralsight/NameFormatterTest.java
+++ src/test/java/com/pluralsight/NameFormatterTest.java
@@ -1,8 +1,40 @@
 package com.pluralsight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameFormatterTest {
-
+    @Test
+    public void format_FirstAndLastNames_ProperFormat(){
+        //Arrange
+        String input = "Arsenii Kunilovskii";
+        //Act
+        input = NameFormatter.format(input);
+        //Assert
+        assertEquals("Kunilovskii Arsenii", input);
+    }
+    @Test
+    public void format_FullName_ProperFormat(){
+        //Arrange
+        String input = "Mr. Arsenii A. Kunilovskii, I";
+        //Act
+        input = NameFormatter.format(input);
+        //Assert
+        assertEquals("Kunilovskii, Mr. Arsenii A., I", input);
+    }
+    @Test
+    public void format_AllValues_ProperFormat(){
+        //Arrange
+        String firstName = "Arsenii";
+        String lastName = "Kunilovskii";
+        String prefix = "Mr.";
+        String middleName = "A.";
+        String suffix = "I";
+        //Act
+        String output = NameFormatter.format(prefix,firstName,middleName,lastName,suffix);
+        //Assert
+        assertEquals("Kunilovskii, Mr. Arsenii A., I", output);
+    }
 
 }
